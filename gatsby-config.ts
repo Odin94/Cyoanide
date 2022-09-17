048383e65597fac7ab20f4ae7330aaceb1edc38a
Odin Kammerloher <kammerloher.andreas@gmail.com>
2022-09-17 14:25:43 +0200

    Use a pretty custom icon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,7 +14,7 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        "icon": "src/images/icon.png"
+        "icon": "src/images/poison-svgrepo-com.svg"
       }
     },
     "gatsby-plugin-image",
